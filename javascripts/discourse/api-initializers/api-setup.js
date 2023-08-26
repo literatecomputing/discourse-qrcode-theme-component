6b9b08c40d377dea31c9d484a58ff43e215f0493
Jay Pfaffman <jay@literatecomputing.com>
2023-08-26 13:23:19 -0500

    include spinner and support chat

--- javascripts/discourse/api-initializers/api-setup.js
+++ javascripts/discourse/api-initializers/api-setup.js
@@ -1,77 +1,94 @@
+import { later } from "@ember/runloop";
 import { apiInitializer } from "discourse/lib/api";
 import loadScript from "discourse/lib/load-script";
-import I18n from "I18n";
 
-async function applyQrcode(element) {
-  const qrcodes = element.querySelectorAll(".d-wrap[data-wrap=qrcode]");
+async function applyQrcode(element, key = "composer") {
+  let qrcodes = element.querySelectorAll("pre[data-code-wrap=qrcode]");
+
   if (!qrcodes.length) {
     return;
   }
 
   await loadScript(settings.theme_uploads_local.qrcode_js);
-
   qrcodes.forEach((qrcode) => {
+    if (qrcode.dataset.processed) {
+      return;
+    }
+
+    const spinner = document.createElement("div");
+    spinner.classList.add("spinner");
+
+    if (qrcode.dataset.codeHeight && key !== "composer") {
+      qrcode.style.height = `${qrcode.dataset.codeHeight}px`;
+    }
+
+    later(() => {
+      if (!qrcode.dataset.processed) {
+        qrcode.append(spinner);
+      }
+    }, 2000);
+  });
+
+  qrcodes = element.querySelectorAll("pre[data-code-wrap=qrcode]");
+  qrcodes.forEach(async (qrcode, index) => {
+    if (qrcode.dataset.processed) {
+      return;
+    }
+    const code = qrcode.querySelector("code");
     const qr = document.createElement("div");
     qr.classList.add("qrcode");
-    const url = qrcode.getAttribute("data-url")
-      ? qrcode.getAttribute("data-url")
-      : window.location.href;
-    /*eslint no-new: 0 */
+    const url = code.innerHTML ? code.innerHTML : window.location.href;
     new window.QRCode(qr, url);
-    let node = document.createTextNode(url);
 
-    qrcode.appendChild(node);
-    qrcode.append(qr);
+    code.innerHTML = null;
+    qrcode.dataset.processed = "true";
+    qrcode.appendChild(qr);
   });
 }
 
-export default apiInitializer("0.11.1", (api) => {
-  const { iconNode } = require("discourse-common/lib/icon-library");
-  iconNode("qrcode");
+let messageSeq = 0;
+let resolvers = {};
 
-  const currentLocale = I18n.currentLocale();
-  // I18n.translations[currentLocale].js.qrcode_button_title = I18n.t(themePrefix("composer_footnote_button_title"));
-  // I18n.translations[currentLocale].js.composer.qrcode_button_text = I18n.t(themePrefix("composer_footnote_button_text"));
-  I18n.translations[currentLocale].js.qrcode_button_title = "Insert QR Code";
-  I18n.translations[currentLocale].js.composer.qrcode_button_text =
-    "Insert QR Code";
+export default apiInitializer("0.11.1", (api) => {
+  api.addToolbarPopupMenuOptionsCallback(() => {
+    return {
+      action: "insertqrcodesample",
+      icon: "qrcode",
+      label: themePrefix("insert_qrcode"),
+    };
+  });
+
+  // this is a hack as applySurround expects a top level
+  // composer key, not possible from a theme
+  window.I18n.translations[
+    window.I18n.locale
+  ].js.composer.qrcode_text = `delete this for URL of current page`;
 
   api.modifyClass("controller:composer", {
-    pluginId: "qrcode",
-
+    pluginId: "discourse-qrcode",
     actions: {
-      qrcodeButton() {
-        this.get("toolbarEvent").addText(
-          "\n" + `[wrap=qrcode url=]` + "\n[/wrap]\n"
+      insertqrcodesample() {
+        this.toolbarEvent.applySurround(
+          "\n```qrcode\n",
+          "```\n",
+          "qrcode_text",
+          { multiline: false }
         );
       },
     },
   });
 
-  api.addToolbarPopupMenuOptionsCallback(() => {
-    return {
-      icon: "qrcode",
-      label: "qrcode_button_title",
-      action: "qrcodeButton",
-    };
-  });
-
-  // add button rather than to the menu
-  // api.onToolbarCreate((toolbar) => {
-  //   toolbar.addButton({
-  //     id: "composer_qrcode",
-  //     group: "extras",
-  //     icon: "qrcode",
-  //     title: "qrcode_button_text",
-  //     perform: (e) => e.addText("\n" + `[wrap=qrcode url=]` + "\n[/wrap]\n"),
-  //   });
-  // });
+  if (api.decorateChatMessage) {
+    api.decorateChatMessage((element) => {
+      applyQrcode(element, `chat_message_${element.id}`);
+    });
+  }
 
   api.decorateCookedElement(
     async (elem, helper) => {
       const id = helper ? `post_${helper.getModel().id}` : "composer";
       applyQrcode(elem, id);
     },
-    { id: "discourse-qrcode-theme-component" }
+    { id: "discourse-qrcode" }
   );
 });
